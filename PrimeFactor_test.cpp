925cf9f2a1b7f295b14f48f1e7b1c2cc95d3bfa8
matzmoto <lemon_bj@naver.com>
2025-03-27 15:34:31 +0900

    7th

--- PrimeFactor_test.cpp
+++ PrimeFactor_test.cpp
@@ -60,114 +60,17 @@ TEST_F(PrimeFixture, Of9)
 }
 
 
+TEST_F(PrimeFixture, Of12)
+{
+	expected = {2, 2, 3};
+
+	EXPECT_EQ(expected, prime_Factor.of(12));
+}
+
+
 int main()
 {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
 
-
-#if 0 
-TEST(AccountTest, AccountT1)
-{
-	Account account(10000);
-	int ret = account.getBalance();
-
-	EXPECT_EQ(10000, ret);
-}
-
-TEST(AccountTest, AccountT2)
-{
-	Account account(10000);
-	account.deposit(500);
-	int ret = account.getBalance();
-
-	EXPECT_EQ(10500, ret);
-}
-TEST(AccountTest, AccountT3)
-{
-	Account account(10000);
-	account.withdraw(600);
-	int ret = account.getBalance();
-
-	EXPECT_EQ(9400, ret);
-}
-
-TEST(AccountTest, AccountT4)
-{
-	Account account(10000, 1.05);
-	account.updateInterest();
-	int ret = account.getBalance();
-
-	EXPECT_EQ(10500, ret);
-}
-
-TEST(AccountTest, AccountT5)
-{
-	Account account(10000, 1.05);
-	account.setInterest(1.1);
-	account.updateInterest();
-	int ret = account.getBalance();
-
-	EXPECT_EQ(11000, ret);
-}
-
-TEST(AccountTest, AccountT6)
-{
-	Account account(10000, 1.1);
-	account.updateInterest();
-	account.updateInterest();
-	int ret = account.getBalance();
-
-	EXPECT_EQ(12100, ret);
-}
-
-TEST(AccountTest, AccountT7)
-{
-	Account account(10000, 1.1);
-	account.updateInterestNyears(5);
-	int ret = account.getBalance();
-
-	EXPECT_EQ(16105, ret);
-}
-
-TEST(AccountTest, AccountT8)
-{
-	Account account(10000, 1.1);
-	int ret = account.expectInterestNyears(5);
-
-	EXPECT_EQ(16105, ret);
-}
-
-
-TEST(cal, calT1)
-{
-	EXPECT_EQ(1, cal().calMinus(5, 4));
-}
-
-TEST(cal, calT2)
-{
-	EXPECT_EQ(9, cal().calMinus(1, 10));
-}
-
-TEST(Fibo, FiboT1)
-{
-	EXPECT_EQ(0, Fibo().fibo(0));
-	EXPECT_EQ(1, Fibo().fibo(1));
-}
-
-
-TEST(Fibo, OrderT)
-{
-	vector<vector<int>> cases = { {0,0}, {1,1} , {3,2} };
-
-	for (int i = 0; i < cases.size(); i++)
-	{
-		int input = cases[i][0];
-		int expected = cases[i][1];
-		EXPECT_EQ(expected, Fibo().fibo(input));
-	}
-}
-
-
-#endif
