7187895f7624e6d7fb2809392c56b8edbfe2a97a
matzmoto <lemon_bj@naver.com>
2025-03-27 15:21:04 +0900

    3rd

--- PrimeFactor.cpp
+++ PrimeFactor.cpp
@@ -1,1 +1,22 @@
 ﻿// ㅎㅎㅎㅎ 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class PrimeFactor
+{
+public:
+	vector<int> of(int number)
+	{
+		vector<int> result = {};
+		if (number > 1)
+		{
+			result.push_back(number);
+		}
+		return result;
+	}
+private:
+
+};
--- PrimeFactor_test.cpp
+++ PrimeFactor_test.cpp
@@ -1,12 +1,47 @@
 ﻿#include "gmock/gmock.h"
+#include "PrimeFactor.cpp";
 
 using namespace std;
+
+class PrimeFixture : public testing::Test
+{
+public:
+	PrimeFactor prime_Factor;
+	vector<int> expected;
+private:
+
+};
+
+
+TEST_F(PrimeFixture, Of1)
+{
+	expected = {};
+
+	EXPECT_EQ(expected, prime_Factor.of(1));
+}
+
+
+TEST_F(PrimeFixture, Of2)
+{
+	expected = {2};
+
+	EXPECT_EQ(expected, prime_Factor.of(2));
+}
+
+TEST_F(PrimeFixture, Of3)
+{
+	expected = {3};
+
+	EXPECT_EQ(expected, prime_Factor.of(3));
+}
+
 
 int main()
 {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
+
 
 #if 0 
 TEST(AccountTest, AccountT1)
